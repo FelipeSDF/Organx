a987a9c14b7e35eb923f73107fd9b552ae7a3f0b
Felipe Sodre de Freitas <felipesodre01e@gmail.com>
2024-08-10 11:31:56 -0300

    routes and bootstrap system

--- Organxx/Organx/views.py
+++ Organxx/Organx/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 
+def page(request, page):
+    
+    if page == 'home':
+        return render(request, 'home.html', {})
+    if page == 'cadastrar':
+        return render(request, 'add.html', {})
+    if page == 'consultar':
+        return render(request, 'consultar.html', {})
+    if page == 'alterar':
+        return render(request, 'alterar.html', {})
+    if page == 'login':
+        return render(request, 'login.html', {})
+
 def home(request):
     return render(request, 'home.html', {})
 
--- Organxx/Organxx/urls.py
+++ Organxx/Organxx/urls.py
@@ -1,28 +1,10 @@
-"""
-URL configuration for Organxx project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-import Organx.views
+import Organx.views as views
 from django.urls import path
 
 urlpatterns = [
-    path('', Organx.views.home, name='home'),
-
-    path('land/', Organx.views.land_page, name='land'),
-    path('add/', Organx.views.add_page, name='add'),
-
+    path('', views.home, name='land'),
+    path('<page>', views.page, name='page'),
+    path('add/', views.add_page, name='add'),
     path('admin/', admin.site.urls),
 ]
